efdc1e53059d17abab324751b5b0e924ad317828
Ahmed Fahmy <ahmedf@Ahmeds-MacBook-Air-2.local>
2024-11-04 16:04:29 -0500

    Your commit message

--- careerclimb/urls.py
+++ careerclimb/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 def profile(request):
+    return HttpResponse('Profile Page')
     
-
 urlpatterns = [
     path('admin/', admin.site.urls),  # For the admin interface
-    path('', include('myapp.urls')),  # Includes URLs from the `myapp` app
+    path('', profile),  # Includes URLs from the `myapp` app
 ]
