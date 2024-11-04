444fa4ac1e6d4ae2887ee906545bd721b9b0bb3e
Ahmed Fahmy <ahmedf@Ahmeds-MacBook-Air-2.local>
2024-11-04 16:34:40 -0500

    added urls.py and views.py for the other htmls

--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,18 @@ def profile(request):
 def room(request):
     return HttpResponse('Room')
 
+# Add job listing view
+def add_job_listing(request):
+    return render(request, 'addJobListing.html')
+
+# Edit job listing view
+def edit_job_listing(request):
+    return render(request, 'editJobListing.html')
+
+# Individual job listing view
+def individual_job_listing(request):
+    return render(request, 'individualJobListing.html')
+
+# Resume advisor view
+def resume_advisor(request):
+    return render(request, 'ResumeAdvisor.html')
