83faa490af3dc46e435a707ddea980e5f511fec7
emilynnguyen1 <emilynnguyen1@gmail.com>
2024-11-21 23:48:37 -0500

    CSS updates for aligning all pages, updating links

--- myapp/views.py
+++ myapp/views.py
@@ -60,7 +60,6 @@ def add_job_listing(request):
             notes=notes
         )
 
-        # *********************** NEEDS TO BE EDITED ****************
         return redirect('individual_job_listing', job_id=job_listing.id)
 
     return render(request, 'addJobListing.html')
@@ -95,9 +94,9 @@ def delete_job_listing(request, job_id):
     job = get_object_or_404(JobListing, id=job_id)
     # Delete the job object
     job.delete()
-    # *********************** NEEDS TO BE EDITED ****************
+
     # Redirect to dashboard after deletion
-    return redirect('profile')
+    return redirect('home')
     
 
 # Individual job listing view
